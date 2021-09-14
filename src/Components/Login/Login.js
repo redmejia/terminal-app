f52fe47d8e745306bd945dd1c809140ea3cabf67
redmejia <preynaldorod@gmail.com>
2021-09-14 13:33:29 -0700

    small changes add bootstrap cdn

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,12 +3,14 @@ const Login = () => {
 	return (
 		<div className="home">
 			<div className="home__box">
-				<div className="row">
-					<div className="col-1-of-2 home__box-rg">
-						one
-					</div>
-					<div className="col-1-of-2 home__box-lft">
-						two
+				<div class="container">
+					<div class="row">
+						<div class="col home__box-rg">
+							1 of 2
+						</div>
+						<div class="col">
+							2 of 2
+						</div>
 					</div>
 				</div>
 			</div>
@@ -17,3 +19,16 @@ const Login = () => {
 }
 
 export default Login;
+
+// <div className="home">
+// <div className="home__box">
+// 	<div className="row">
+// 		<div className="col-1-of-2 home__box-rg">
+// 			one
+// 		</div>
+// 		<div className="col-1-of-2 home__box-lft">
+// 			two
+// 		</div>
+// 	</div>
+// </div>
+// </div>
