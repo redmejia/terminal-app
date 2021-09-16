623a6a6392619760cc98183be677c99f6782c0f9
redmejia <preynaldorod@gmail.com>
2021-09-15 22:24:09 -0700

    Register

---
+++ src/Components/Regiter/Register.js
@@ -0,0 +1,20 @@
+const Register = () => {
+	return (
+		<div className="home">
+			<div className="home__box">
+				<div class="container">
+					<div class="row">
+						<div class="col home__box-rg">
+							1 of 2
+						</div>
+						<div class="col">
+							2 of 2
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Register;
