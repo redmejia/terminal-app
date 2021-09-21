4138c6595e2f5bf22d7629f5cf4ca0d8a2168250
redmejia <preynaldorod@gmail.com>
2021-09-21 00:58:58 -0700

    styling log in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import './App.scss';
 import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
+// import Register from './Components/Register/Register';
 import './Components/Styles/main.scss'
 
 function App() {
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,36 +1,58 @@
 
 const Login = () => {
+	const data = {
+		projects: 53,
+		topProjects: 35,
+		developers: 153,
+	}
 	return (
 		<div className="home">
 			<div className="home__box">
-				<div class="container">
-					<div class="row">
-						<div class="col home__box-rg">
-							<img width="80" src="https://cdn.jsdelivr.net/npm/simple-icons@v5/icons/iterm2.svg" />
+				<div class="row">
+					<div class="col home__box-rg">
+						{/* 
+						<div className="side-box">
+							<img width="180" src="https://cdn.jsdelivr.net/npm/simple-icons@v5/icons/iterm2.svg" alt="logo" />
+						</div> */}
+						<img width="180" src="https://cdn.jsdelivr.net/npm/simple-icons@v5/icons/iterm2.svg" alt="logo" />
+						<div className="form-box">
+
+							<h2 className="heading-primary"> Hello World</h2>
+
+							<h3 className="heading-secundary">You are not log in yet! help the next project become top project</h3>
+
+							<h3 className="heading-secundary">More than <span className="remark-text"> {data.projects} </span> projects</h3>
+							<h3 className="heading-secundary">More than <span className="remark-text"> {data.topProjects} </span> top projects</h3>
+							<h3 className="heading-secundary">More than <span className="remark-text"> {data.developers} </span> developers colavorating</h3>
 						</div>
-						<div class="col home__box-lft">
-							{/* <form onSubmit={e => userSignin(e)}> */}
-							<div className="form-box">
 
-								<form>
+					</div>
+					<div class="col home__box-lft">
+						{/* <form onSubmit={e => userSignin(e)}> */}
+						{/* <img width="180" src="https://cdn.jsdelivr.net/npm/simple-icons@v5/icons/iterm2.svg" alt="logo" /> */}
 
-									<input
-										className="form-box__inputs"
-										type="text"
-										placeholder="email"
-									// onChange={onHandle_Email}
-									/>
+						<div className="form-box">
+							<h2 className="heading-primary"> Log In </h2>
 
-									<input
-										className="form-box__inputs"
-										type="password"
-										placeholder="password"
-										autoComplete="no"
-									// onChange={onHandle_Pwd}
-									// onKeyDown={userSignin}
-									/>
-								</form>
-							</div>
+
+							<form>
+
+								<input
+									className="form-box__inputs"
+									type="text"
+									placeholder="email"
+								// onChange={onHandle_Email}
+								/>
+
+								<input
+									className="form-box__inputs"
+									type="password"
+									placeholder="password"
+									autoComplete="no"
+								// onChange={onHandle_Pwd}
+								// onKeyDown={userSignin}
+								/>
+							</form>
 						</div>
 					</div>
 				</div>
