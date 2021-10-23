19b28594aea063b08351fa58a0d6a0b1cfc09896
redmejia <preynaldorod@gmail.com>
2021-10-22 22:28:01 -0700

    paragrah and titles style

--- src/Components/Utils/Card.js
+++ src/Components/Utils/Card.js
@@ -4,7 +4,8 @@ const Card = (props) => {
 		<div className="card" >
 			<img className="card-img-top" src={sample} alt="sample" />
 			<div className="card-body">
-				<h5 className="heading-primary">{props.title}</h5>
+				{/* <h5 className="heading-primary">{props.title}</h5> */}
+				<p className="title" >{props.title}</p>
 				<p className="paragraph">{props.description}</p>
 				{/* <a href="#" className="btn btn-primary">++</a> */}
 				<a href="#/" className="btn btn--green">++</a>
